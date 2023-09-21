9b3a8fd97abd153dd806fd35934932851c05af7f
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-21 14:04:40 +0200

    make it boot into the actual thing

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
   outputs = {self, nixpkgs }: {
-    nixosConfigurations.potsdam-pnp = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.runner = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [ ./networking.nix ./hardware-configuration.nix ./runner.nix ];
