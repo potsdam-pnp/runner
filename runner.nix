0ba19a084e7826833c6c7b8a82aca45cb7c0342e
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-22 00:12:12 +0200

    add github runner

---
+++ runner.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  services.github-runner = {
+    enable = true;
+    url = "https://github.com/potsdam-pnp";
+    tokenFile = pkgs.writeText "tok" "AACWRHZPW5QL7MYIN2RSD7TFBTGMQ"; 
+  };
+}
