ca198001ede0b379380c2250e9c441a964337c83
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-22 11:46:34 +0200

    use newer github runner

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs";
 
-  outputs = {self, nixpkgs }: {
+  outputs = {self, nixpkgs, nixpkgs-unstable }: {
     nixosConfigurations.runner = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
@@ -10,6 +11,7 @@
         nixpkgs.config.permittedInsecurePackages = [
           "nodejs-16.20.2"
         ];
+        services.github-runner.package = nixpkgs-unstable.legacyPackages.x86_64-linux.github-runner;
       }];
     };
   };
