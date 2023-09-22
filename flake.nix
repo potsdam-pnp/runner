9149bcf8aa7278d5519989d7a81c9687083e74ef
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-22 12:52:00 +0200

    move settings around

--- flake.nix
+++ flake.nix
@@ -6,12 +6,7 @@
     nixosConfigurations.runner = nixpkgs-unstable.lib.nixosSystem {
       system = "x86_64-linux";
 
-      modules = [ ./configuration.nix ./runner.nix {
-        nix.settings.experimental-features = [ "nix-command" "flakes" ];
-        nixpkgs.config.permittedInsecurePackages = [
-          "nodejs-16.20.2"
-        ];
-      }];
+      modules = [ ./configuration.nix ./runner.nix ]
     };
   };
 }
--- runner.nix
+++ runner.nix
@@ -5,4 +5,8 @@
     tokenFile = builtins.toFile "tok" "AACWRH3HQMTGR6P5TPAXRVLFBVSNA"; 
     nodeRuntimes = [ "node16" "node20" ];
   };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "nodejs-16.20.2"
+  ];
 }
