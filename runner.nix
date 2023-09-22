015a24146b87840adf5e5a8fc05de205034419f3
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-22 12:24:47 +0200

    add node16 runtime (needed for github-script)

--- runner.nix
+++ runner.nix
@@ -3,5 +3,6 @@
     enable = true;
     url = "https://github.com/potsdam-pnp";
     tokenFile = builtins.toFile "tok" "AACWRH3HQMTGR6P5TPAXRVLFBVSNA"; 
+    nodeRuntimes = [ "node16" "node20" ];
   };
 }
