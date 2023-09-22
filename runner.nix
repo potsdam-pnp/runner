5913fcd517ce3a793e679db5bd03fb4d2355d87b
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-22 13:58:40 +0200

    update token

--- runner.nix
+++ runner.nix
@@ -2,7 +2,7 @@
   services.github-runner = {
     enable = true;
     url = "https://github.com/potsdam-pnp";
-    tokenFile = builtins.toFile "tok" "AACWRH3HQMTGR6P5TPAXRVLFBVSNA"; 
+    tokenFile = builtins.toFile "tok" "AACWRHY5QB7EWOSUN5DHSQTFBWHXA"; 
     nodeRuntimes = [ "node16" "node20" ];
   };
   nix.settings = {
